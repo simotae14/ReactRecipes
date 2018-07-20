2b2b92705456df8eeae0ea22fc05a9fc0c261d59
Simone Taeggi <simone.taeggi@leovegas.com>
2018-07-20 19:54:42 +0200

    Setup the connection to a MongoDB database

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config({
+    path: 'variables.env'
+});
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log('DB connected'))
+    .catch(err => console.error(err));
 
 const app = express();
+
 
 const PORT = process.env.PORT || 4444;
 
