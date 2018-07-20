5cd459cf8c0093fae2dbfbffb050c820a74fde68
Simone Taeggi <simone.taeggi@leovegas.com>
2018-07-20 19:31:00 +0200

    Added and setup the server file

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 4444;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on PORT ${PORT}`);
+});
