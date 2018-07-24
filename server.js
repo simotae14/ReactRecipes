19f70fde18d511399be6103a07344b564e9994fd
simotae14 <startae14@gmail.com>
2018-07-24 23:43:30 +0200

    partial solution GraphQL setup

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 require('dotenv').config({
     path: 'variables.env'
 });
 const Recipe = require('./models/Recipe');
 const User = require('./models/User');
+
+// Recupera il middleware GraphQL-Express
+const { graphiqlExpress, graphqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
+const { typeDefs } = require('./schema');
+const { resolvers } = require('./resolvers');
+
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+});
 
 // Connects to database
 mongoose
@@ -15,6 +27,8 @@ mongoose
 // Initializes application
 const app = express();
 
+// Creo la GraphiQL app
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
 const PORT = process.env.PORT || 4444;
 
