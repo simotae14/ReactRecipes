8b8cedf579f19358fa42b3712f3b0e5ac047d4a9
Simone Taeggi <simone.taeggi@leovegas.com>
2018-07-25 17:05:40 +0200

    Add bodyParser Middleware and Root Query Type

--- server.js
+++ server.js
@@ -30,12 +30,15 @@ const app = express();
 // Creo la GraphiQL app
 app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 // Connetto gli Schema a GraphQL
-app.use('/graphql', graphqlExpress({
-    schema,
-    context: {
-        Recipe,
-        User
-    }
+app.use(
+    '/graphql',
+    bodyParser.json(),
+    graphqlExpress({
+        schema,
+        context: {
+            Recipe,
+            User
+        }
 }));
 
 const PORT = process.env.PORT || 4444;
