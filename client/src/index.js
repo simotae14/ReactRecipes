5fcec5ef84b562939d7ebe422b8b5b4564c221c3
Simone Taeggi <simone.taeggi@leovegas.com>
2018-07-26 23:57:45 +0200

    Setup ApolloClient and ApolloProvider

--- client/src/index.js
+++ client/src/index.js
@@ -3,4 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
 ReactDOM.render(<App />, document.getElementById('root'));
