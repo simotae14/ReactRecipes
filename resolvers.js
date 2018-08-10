6e8257bfb5fb498fad8bb030a6049adfc548c64b
simotae14 <startae14@gmail.com>
2018-08-10 23:35:06 +0200

    Create Signup Mutation

--- resolvers.js
+++ resolvers.js
@@ -1,3 +1,12 @@
+// importo package per le password
+const jwt = require('jsonwebtoken');
+
+// funzione per creare il token criptato
+const createToken = (user, secret, expiresIn) => {
+    const { username, email } = user;
+    return jwt.sign({ username, email },secret, { expiresIn });
+}
+
 exports.resolvers = {
     Query: {
         getAllRecipes: async(root, args, { Recipe }) => {
@@ -15,6 +24,26 @@ exports.resolvers = {
                 username
             }).save();
             return newRecipe;
+        },
+        // creo la nuova mutation che è async, il context è l'User model
+        signupUser: async(root, { username, email, password }, { User }) => {
+            // recupero utente
+            const user = await User.findOne({ username });
+
+            // se trova utente lancio errore
+            if (user) {
+                throw new Error('User already exists');
+            }
+            // altrimenti creo nuovo utente
+            const newUser = await new User({
+                username,
+                email,
+                password
+            }).save();
+            // restituisco il token come da query
+            // la secret la prendo dall'env
+            // scade in 1 ora
+            return { token: createToken(newUser, process.env.SECRET,'1hr' ) };
         }
     }
 };
