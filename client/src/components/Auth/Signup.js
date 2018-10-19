7a1c2d7af3d00c14099013cf2edd638f99416cdb
Simone Taeggi <simone.taeggi@leovegas.com>
2018-10-19 12:06:59 +0200

    Added Form for the Signup

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -3,8 +3,42 @@ import React from 'react';
 class Signup extends React.Component {
     render() {
         return (
-            <div>
-                Signup
+            <div
+                className="App"
+            >
+                <h2
+                    className="App"
+                >
+                    Signup
+                </h2>
+                <form className="form">
+                    <input
+                        type="text"
+                        name="username"
+                        placeholder="Username"
+                    />
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Email Address"
+                    />
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                    />
+                    <input
+                        type="password"
+                        name="passwordConfirmation"
+                        placeholder="Confirm Password"
+                    />
+                    <button
+                        type="submit"
+                        className="button-primary"
+                    >
+                        Submit
+                    </button>
+                </form>
             </div>
         );
     }
