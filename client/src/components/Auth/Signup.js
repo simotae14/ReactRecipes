909e8ab50962f5d0e37d715dff79ef4752ff7633
Simone Taeggi <simone.taeggi@leovegas.com>
2018-10-19 15:45:30 +0200

    Added State for the input fields in the Signup form

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
 class Signup extends React.Component {
+    state = {
+        username: "",
+        email: "",
+        password: "",
+        passwordConfirmation: ""
+    }
+    // handler for the change of the input fields
+    handleChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+    }
     render() {
+        const { username, email, password, passwordConfirmation } = this.state;
         return (
             <div
                 className="App"
@@ -16,21 +30,29 @@ class Signup extends React.Component {
                         type="text"
                         name="username"
                         placeholder="Username"
+                        value={username}
+                        onChange={this.handleChange}
                     />
                     <input
                         type="email"
                         name="email"
                         placeholder="Email Address"
+                        value={email}
+                        onChange={this.handleChange}
                     />
                     <input
                         type="password"
                         name="password"
                         placeholder="Password"
+                        value={password}
+                        onChange={this.handleChange}
                     />
                     <input
                         type="password"
                         name="passwordConfirmation"
                         placeholder="Confirm Password"
+                        value={passwordConfirmation}
+                        onChange={this.handleChange}
                     />
                     <button
                         type="submit"
