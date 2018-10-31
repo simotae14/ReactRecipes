27b06880620a847c287590f8504d7d410f1ddda4
Simone Taeggi <simone.taeggi@leovegas.com>
2018-10-31 14:17:54 +0100

    Verify JWT on BE to get current user

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config({
 	path: "variables.env"
 });
@@ -39,7 +40,15 @@ app.use(cors(corsOptions));
 // Set up JWT authentication middleware
 app.use(async (req, res, next) => {
 	const token = req.headers["authorization"];
-	console.log(token);
+
+	if (token !== "null") {
+		try {
+			const currentUser = await jwt.verify(token, process.env.SECRET);
+			console.log(currentUser);
+		} catch (err) {
+			console.error(err);
+		}
+	}
 	next();
 });
 
